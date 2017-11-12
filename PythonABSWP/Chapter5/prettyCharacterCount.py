f6f184eaaf863abd600caee378e117af01c646ef
PrasadVaradarajanIyer <piyer@nyit.edu>
2017-11-11 19:38:34 -0500

    Changed

---
+++ PythonABSWP/Chapter5/prettyCharacterCount.py
@@ -0,0 +1,9 @@
+import pprint
+message = 'It is a very cold weather today, does not feel like going outside.'
+count={}
+
+for character in message:
+        count.setdefault(character, 0)
+        count[character]=count[character]+1;
+
+pprint.pprint(count)
